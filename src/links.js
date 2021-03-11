f23b2f88349aeac5f1c11fbf4adf197de692029b
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-03-11 18:16:33 +0200

    Update links.js

--- src/links.js
+++ src/links.js
@@ -1,7 +1,12 @@
 const jsonData = {
-  mainInfo: "https://fouad-abdeen.github.io/landing-page-data/main.json",
+  info: "https://fouad-abdeen.github.io/landing-page-data/info.json",
+  urls: "https://fouad-abdeen.github.io/landing-page-data/urls.json",
+  titles: "https://fouad-abdeen.github.io/landing-page-data/titles.json",
+  paragraphs:
+    "https://fouad-abdeen.github.io/landing-page-data/paragraphs.json",
   articles: "https://fouad-abdeen.github.io/landing-page-data/articles.json",
-  miscellaneous: "https://fouad-abdeen.github.io/landing-page-data/miscellaneous.json",
+  miscellaneous:
+    "https://fouad-abdeen.github.io/landing-page-data/miscellaneous.json",
 };
 
 export default jsonData;
