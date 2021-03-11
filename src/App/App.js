2fae72c0f038d408d359da3272ab3c4daea19687
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-03-11 18:15:46 +0200

    Change App to function-based component to benefit from hooks

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./App.scss";
 import jsonData from "../links";
 import Bar from "../Bar/Bar";
@@ -6,144 +6,82 @@ import Header from "../Header/Header";
 import Body from "../Body/Body";
 import Footer from "../Footer/Footer";
 import { Dropdown } from "react-bootstrap";
+import useFetch from "../useFetch";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lang: "ar",
-      error: null,
-      isLoaded: false,
-      info: [],
-      urls: [],
-      titles: [],
-      paragraphs: [],
-      articles: [],
-      miscellaneous: [],
-    };
-  }
+const App = () => {
+  const isComponentMounted = useRef(true);
+  const [lang, setLang] = useState("ar");
 
-  switchToEn = () => {
-    this.setState({ lang: "en" });
+  const info = useFetch(jsonData.info, isComponentMounted, []);
+  const urls = useFetch(jsonData.urls, isComponentMounted, []);
+  const titles = useFetch(jsonData.titles, isComponentMounted, []);
+  const paragraphs = useFetch(jsonData.paragraphs, isComponentMounted, []);
+  const articles = useFetch(jsonData.articles, isComponentMounted, []);
+  const miscellaneous = useFetch(
+    jsonData.miscellaneous,
+    isComponentMounted,
+    []
+  );
+
+  const switchToEn = () => {
+    setLang("en");
   };
 
-  switchToAr = () => {
-    this.setState({ lang: "ar" });
+  const switchToAr = () => {
+    setLang("ar");
   };
 
-  componentDidMount = () => {
-    // Fetching Info Data
-    fetch(jsonData.mainInfo)
-      .then((response) => response.json())
-      .then(
-        (data) => {
-          this.setState({
-            isLoaded: true,
-            info: data.info,
-            urls: data.urls,
-            titles: data.titles,
-            paragraphs: data.paragraphs,
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
-
-    // Fetching Articles Data
-    fetch(jsonData.articles)
-      .then((response) => response.json())
-      .then(
-        (data) => {
-          this.setState({ isLoaded: true, articles: data });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
-
-    // Fetching Misc Data
-    fetch(jsonData.miscellaneous)
-      .then((response) => response.json())
-      .then(
-        (data) => {
-          this.setState({
-            isLoaded: true,
-            miscellaneous: data,
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
-  };
-
-  render() {
-    return (
-      <>
-        <div className="App">
-          <div className="App-bar">
-            <Bar
-              lang={this.state.lang}
-              info={this.state.info}
-              urls={this.state.urls}
-            />
-            <div className="bar-lang">
-              <Dropdown>
-                <Dropdown.Toggle
-                  variant="dark"
-                  id="dropdown-basic"
-                  size="sm"
-                  style={{ color: "#fcac18" }}
-                >
-                  Language
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  {this.state.lang === "ar" ? (
-                    <Dropdown.Item onClick={this.switchToEn}>
-                      <strong> English </strong>
-                    </Dropdown.Item>
-                  ) : (
-                    <Dropdown.Item onClick={this.switchToAr}>
-                      <strong> Arabic </strong>
-                    </Dropdown.Item>
-                  )}
-                </Dropdown.Menu>
-              </Dropdown>
-            </div>
-          </div>
-          <div className="App-header">
-            <Header />
-          </div>
-          <div className="App-body">
-            <Body
-              lang={this.state.lang}
-              articles={this.state.articles}
-              titles={this.state.titles}
-              paragraphs={this.state.paragraphs}
-              urls={this.state.urls}
-            />
-          </div>
-          <div className="App-footer">
-            <Footer
-              lang={this.state.lang}
-              paragraphs={this.state.paragraphs}
-              miscellaneous={this.state.miscellaneous}
-            />
+  return (
+    <>
+      <div className="App">
+        <div className="App-bar">
+          <Bar lang={lang} info={[...info.data]} urls={[...urls.data]} />
+          <div className="bar-lang">
+            <Dropdown>
+              <Dropdown.Toggle
+                variant="dark"
+                id="dropdown-basic"
+                size="sm"
+                style={{ color: "#fcac18" }}
+              >
+                Language
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                {lang === "ar" ? (
+                  <Dropdown.Item onClick={switchToEn}>
+                    <strong> English </strong>
+                  </Dropdown.Item>
+                ) : (
+                  <Dropdown.Item onClick={switchToAr}>
+                    <strong> Arabic </strong>
+                  </Dropdown.Item>
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
           </div>
         </div>
-      </>
-    );
-  }
-}
+        <div className="App-header">
+          <Header />
+        </div>
+        <div className="App-body">
+          <Body
+            lang={lang}
+            articles={[...articles.data]}
+            titles={[...titles.data]}
+            paragraphs={[...paragraphs.data]}
+            urls={[...urls.data]}
+          />
+        </div>
+        <div className="App-footer">
+          <Footer
+            lang={lang}
+            paragraphs={[...paragraphs.data]}
+            miscellaneous={[...miscellaneous.data]}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
